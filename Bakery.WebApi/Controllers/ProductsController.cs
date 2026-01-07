7c2ad76d62b07a3b151182fb1e6e122ccb271ad7
Anna <annapolushkina13@gmail.com>
2026-01-07 03:26:42 +0100

    Add ProductsController and first GET endpoint

---
+++ Bakery.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Bakery.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery.WebApi.Controllers
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product("Bread", 2.50m),
+                new Product("Croissant", 1.80m),
+                new Product("Cake", 15.00m)
+            };
+
+            return Ok(products);
+        }
+    }
+}
+
