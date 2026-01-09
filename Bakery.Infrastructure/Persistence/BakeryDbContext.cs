26f3935e20a2119d9cdccd7e3186ba6668580298
Anna <annapolushkina13@gmail.com>
2026-01-09 05:40:37 +0100

    Add BakeryDbContext to Infrastructure

---
+++ Bakery.Infrastructure/Persistence/BakeryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bakery.Core;
+using Bakery.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery.Infrastructure.Persistence;
+
+public class BakeryDbContext : DbContext
+{
+    public BakeryDbContext(DbContextOptions<BakeryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
