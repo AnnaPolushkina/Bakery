473d197f4cee65535cad230a8f47efef8bb1b9b4
Anna <annapolushkina13@gmail.com>
2026-01-16 01:49:01 +0100

    Add IOrderService contract

---
+++ Bakery.Core/Services/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using Bakery.Core.Entities;
+
+namespace Bakery.Core.Services;
+
+public interface IOrderService
+{
+    Task<Order> CreateAsync(Guid clientId);
+
+    Task AddItemAsync(
+        Guid orderId,
+        Guid productId,
+        decimal price,
+        int quantity);
+
+    Task ConfirmAsync(Guid orderId);
+
+    Task<IEnumerable<Order>> GetByClientAsync(Guid clientId);
+}
+
